6cc4e693f8c6672a94d98142659534921332d0b3
fang cheng <www.tkzc@foxmail.com>
2024-05-10 18:05:22 +0800

    compile success

--- cmake/Configuration.cmake
+++ cmake/Configuration.cmake
@@ -31,51 +31,19 @@
      endif()
  endif()
 
-# set platform specific path
-set(_path_prefix ${CMAKE_CURRENT_SOURCE_DIR}/prebuilt/)
-if(IOS)
-    if(TVOS)
-        set(platform_name tvos)
-        set(platform_spec_path tvos)
-    else()
-        set(platform_name ios)
-        set(platform_spec_path ios)
-    endif()
-elseif(ANDROID)
-    set(platform_name android)
-    set(platform_spec_path android/${ANDROID_ABI})
-elseif(WINDOWS)
-    set(platform_name windows)
-    if(WIN64)
-        set(platform_spec_path windows/x64)
-    else()
-        set(platform_spec_path windows/x86)
-    endif()
-elseif(MACOSX)
-    set(platform_name mac)
-    set(platform_spec_path mac)
-elseif(LINUX)
-    set(platform_name linux)
-    set(platform_spec_path linux/x64)
-endif()
-
-set(platform_spec_path "${_path_prefix}${platform_spec_path}")
-
-
-
 set(CMAKE_CXX_STANDARD 20)
 set(CXX_STANDARD_FLAG -std=c++20)
 
-if(MSVC)     
-    # Use the static C library for all build types
-    foreach(var 
-        CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
-        CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO
-        CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
-        CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO
-      )
-      if(${var} MATCHES "/MD")
-        string(REGEX REPLACE "/MD" "/MT" ${var} "${${var}}")
-      endif()
-    endforeach()    
-endif(MSVC)
+#if(MSVC)     
+#    # Use the static C library for all build types
+#    foreach(var 
+#        CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+#        CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO
+#        CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
+#        CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO
+#      )
+#      if(${var} MATCHES "/MD")
+#        string(REGEX REPLACE "/MD" "/MT" ${var} "${${var}}")
+#      endif()
+#    endforeach()    
+#endif(MSVC)
--- main.cpp
+++ main.cpp
@@ -1,7 +1,86 @@
 #include "sol/sol.hpp"
+#include <iostream>
+
+#define IUPLUA_NO_GL 1
+#define IUPLUA_NO_CD 1
+
+#include <iup.h>
+#include "iuplua.h"
+
+// gl
+#if !IUPLUA_NO_GL
+#include "iupgl.h"
+#include "iupglcontrols.h"
+#include "iupluagl.h"
+#include "iupluaglcontrols.h"
+#endif
+// im
+//#include "iupluaim.h"
+// tuio
+#include "iupluatuio.h"
+// web
+#include "iupluaweb.h"
+// scintilla
+#include "iuplua_scintilla.h"
+
+#if !IUPLUA_NO_CD
+// controls
+#include "iupcontrols.h"
+#include "iupluacontrols.h"
+// plot
+#include "iup_plot.h"
+#include "iuplua_plot.h"
+#endif
 
 int main(int argc, char** argv)
 {
 	sol::state lua;
 	lua.open_libraries();
+
+    auto L = lua.lua_state();
+    iuplua_open(L);
+    // gl
+#if !IUPLUA_NO_GL
+    iupgllua_open(L);
+    iupglcontrolslua_open(L);
+#endif
+    // im
+    //iupimlua_open(L);
+    // tuio
+    iuptuiolua_open(L);
+    // web
+    iupweblua_open(L);
+    // scintilla
+    iup_scintillalua_open(L);
+#if !IUPLUA_NO_CD
+    // controls
+    iupcontrolslua_open(L);
+    // plot
+    iup_plotlua_open(L);
+#endif
+
+    sol::load_result script = lua.load("main.lua");
+
+    // 检查文件是否加载成功
+    if (!script.valid()) 
+    {
+        sol::error err = script;
+        // 文件加载失败，打印错误消息
+        std::cerr << "Error loading Lua script: " << err.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // 执行 Lua 脚本
+    sol::protected_function_result result = script();
+
+    // 检查执行结果
+    if (!result.valid()) 
+    {
+        sol::error err = result;
+        // 打印错误消息
+        std::cerr << "Error executing Lua script: " << err.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+	return EXIT_SUCCESS;
 }
